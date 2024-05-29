d388c6d479458f28a049ff67c3f7a18d011a1250
vishag002 <vvishag477@gmail.com>
2024-05-29 22:02:57 +0530

    text_constant

---
+++ lib/utilis/text_const.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextConst {
+  static TextStyle heading = GoogleFonts.aBeeZee(
+      textStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  ));
+  static TextStyle description = GoogleFonts.inconsolata(
+      textStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 15,
+    fontWeight: FontWeight.w600,
+  ));
+  static TextStyle authorName = GoogleFonts.inconsolata(
+      textStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 13,
+    fontWeight: FontWeight.bold,
+  ));
+}
