cacc44981d727e4b25e4fb37fa5f158fc6a9bdcd
vishag002 <vvishag477@gmail.com>
2024-05-30 20:11:24 +0530

    color_constant

--- lib/utilis/color_const.dart
+++ lib/utilis/color_const.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class ColorConst {
   static Color background = Colors.black;
+  static Color primary = Colors.white;
+  static Color primary1 = Colors.grey.shade600;
 }
