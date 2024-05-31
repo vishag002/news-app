cc30b2b851031a81f6bdf875280d2508ee8881a4
vishag002 <vvishag477@gmail.com>
2024-05-31 18:34:57 +0530

    detail view ui

--- lib/view/news_view.dart
+++ lib/view/news_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/utilis/text_const.dart';
 
 class NewsViewScreen extends StatelessWidget {
   const NewsViewScreen({super.key});
@@ -6,17 +8,70 @@ class NewsViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        toolbarHeight: 80,
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_ios_rounded,
+              size: 35,
+              color: Colors.black,
+            )),
+        title: Text("NewsX",
+            style: GoogleFonts.abyssinicaSil(
+              textStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.w600,
+              ),
+            )),
+        //search button
+      ),
       body: Column(
         children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            color: Colors.amber,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/rolls_royce_phantom_top_10.jpg?itok=XjL9f1tx"), // Replace with a valid image URL
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
           ),
-          Text("News Title will be shown here"),
           Text(
-              "News Desciption will be shown here News Desciption will be shown hereNews Desciption will be shown hereNews Desciption will be,,News Desciption will be shown hereNews Desciption will be shown here")
+            "News Title will be shown here",
+            style: TextConst.heading,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              "News Description will be shown here. News Description will be shown here. "
+              "News Description will be shown here. News Description will be shown here. "
+              "News Description will be shown here. News Description will be shown here.",
+              style: TextConst.description,
+            ),
+          )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        onPressed: () {},
+        child: Icon(
+          Icons.favorite_border,
+          color: Colors.red,
+          size: 50,
+        ),
       ),
     );
   }
