4c6ff1adb0aa3f1c7faee154d894bffdeb8807f2
vishag002 <vvishag477@gmail.com>
2024-05-31 16:12:36 +0530

    search_controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/controller/home_screen_controler.dart';
+import 'package:news_app/controller/search_controller.dart';
 import 'package:news_app/view/botttom_nav_bar_screen.dart';
-import 'package:news_app/view/home_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
       create: (context) => HomeScreenController(),
-    )
+    ),
+    ChangeNotifierProvider(
+      create: (context) => SearchPageController(),
+    ),
   ], child: MyApp()));
 }
 
