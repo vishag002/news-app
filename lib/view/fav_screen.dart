d8844965838912209a3a53f65a03e7187b89d54b
vishag002 <vvishag477@gmail.com>
2024-06-01 16:29:42 +0530

    ui

--- lib/view/fav_screen.dart
+++ lib/view/fav_screen.dart
@@ -1,168 +1,90 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:news_app/controller/search_controller.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/utilis/color_const.dart';
 import 'package:news_app/utilis/text_const.dart';
-import 'package:provider/provider.dart';
 
-class SearchScreen extends StatefulWidget {
-  const SearchScreen({super.key});
-
-  @override
-  State<SearchScreen> createState() => _SearchScreenState();
-}
-
-class _SearchScreenState extends State<SearchScreen> {
-  final searchViewController = TextEditingController();
-  /* @override
-  void initState() {
-    getData();
-    super.initState();
-  } */
-
-  //
-  /* Future<void> getData() async {
-   
-  } */
+class FavouritesScreen extends StatelessWidget {
+  const FavouritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final searchPageController = Provider.of<SearchPageController>(context);
-
     return Scaffold(
-        appBar: AppBar(
-          foregroundColor: Colors.white,
-          toolbarHeight: 80,
-          backgroundColor: Colors.black,
-          title: Padding(
-            padding: const EdgeInsets.all(28.0),
-            child: TextField(
-              //controller from provider
-              controller: searchViewController,
-
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-              ),
-              textAlign: TextAlign.justify,
-              cursorColor: Colors.white,
-              showCursor: false,
-              decoration: InputDecoration(
-                // enabledBorder: InputBorder.none,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(
-                    color: Colors.white,
-                    width: 1,
-                  ),
-                  gapPadding: 50,
-                ),
-                focusColor: Colors.white,
-                hoverColor: Colors.white,
-                fillColor: Colors.amber,
-                hintText: "Search here...",
-                hintStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-                floatingLabelAlignment: FloatingLabelAlignment.center,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white,
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                alignLabelWithHint: true,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
+      appBar: AppBar(
+        centerTitle: true,
+        toolbarHeight: 80,
+        backgroundColor: ColorConst.background,
+        title: Text(
+          "NewsX",
+          style: GoogleFonts.abyssinicaSil(
+            textStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 40,
+              fontWeight: FontWeight.w600,
             ),
           ),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Provider.of<SearchPageController>(context, listen: false)
-                      .searchData(
-                          searchingData:
-                              searchViewController.text.toLowerCase());
-                },
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.white,
-                  size: 30,
-                  weight: 100,
-                ))
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                // color: Colors.amberAccent,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Container(
+                        width: 140,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/rolls_royce_phantom_top_10.jpg?itok=XjL9f1tx"), // Replace with a valid image URL
+                            fit: BoxFit.cover,
+                            scale: 1,
+                          ),
+                        ),
+                        // color: Colors.black,
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(height: 10),
+                        Container(
+                          //   transformAlignment: Alignment.,
+                          height: 135,
+                          width: 220,
+                          // color: Colors.cyan,
+                          child: Text(
+                            "display the titl here display the titl here display the titl here ",
+                            overflow: TextOverflow.clip,
+                            style: TextConst.heading,
+                            textAlign: TextAlign.justify,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Text(
+                            "name author",
+                            overflow: TextOverflow.ellipsis,
+                            style: TextConst.authorName,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
-        body: ListView.builder(
-          itemCount: searchPageController.myObjClass.totalResults,
-          itemBuilder: (context, index) => Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 200,
-                  width: double.infinity,
-                  // color: Colors.amberAccent,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Container(
-                          width: 140,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage(searchPageController
-                                      .myObjClass.articles?[index].urlToImage ??
-                                  ""),
-                              fit: BoxFit.cover,
-                              scale: 1,
-                            ),
-                          ),
-                          // color: Colors.black,
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(height: 10),
-                          Container(
-                            //   transformAlignment: Alignment.,
-                            height: 135,
-                            width: 220,
-                            // color: Colors.cyan,
-                            child: Text(
-                              searchPageController
-                                      .myObjClass.articles?[index].title ??
-                                  "",
-                              overflow: TextOverflow.clip,
-                              style: TextConst.heading,
-                              textAlign: TextAlign.justify,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              searchPageController
-                                      .myObjClass.articles?[index].author ??
-                                  "",
-                              overflow: TextOverflow.ellipsis,
-                              style: TextConst.authorName,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
