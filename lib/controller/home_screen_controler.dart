e14ade516c22ad58ba13187bbbac11e31cdfa9f3
vishag002 <vvishag477@gmail.com>
2024-06-01 16:28:04 +0530

    added drawer

--- lib/controller/home_screen_controler.dart
+++ lib/controller/home_screen_controler.dart
@@ -12,9 +12,7 @@ class HomeScreenController with ChangeNotifier {
     isloading = true;
     notifyListeners();
     final url = Uri.parse(
-        "https://newsapi.org/v2/everything?q=tesla&from=2024-04-30&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770"
-        // "https://newsapi.org/v2/everything?q=tesla&from=2024-04-29&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770",
-        );
+        " https://newsapi.org/v2/everything?q=tesla&from=2024-05-01&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770");
 
     final response = await http.get(url);
     if (response.statusCode == 200) {
