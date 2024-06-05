84b0873e9c234865b04cbf60ac52479ed577b54c
vishag002 <vvishag477@gmail.com>
2024-06-05 11:11:56 +0530

    removed profile page and fav pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:news_app/controller/category_controller.dart';
 import 'package:news_app/controller/home_screen_controler.dart';
 import 'package:news_app/controller/search_controller.dart';
-import 'package:news_app/view/botttom_nav_bar_screen.dart';
+import 'package:news_app/view/home_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -26,7 +26,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavBarScree(),
+      home: HomeScreen(),
+      //home: NavBarScree(),
     );
   }
 }
