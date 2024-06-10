d5841fc56e0afa85509f5bc5ff998f4f34653fc4
vishag002 <vvishag477@gmail.com>
2024-06-10 18:10:52 +0530

    api update

--- lib/controller/home_screen_controler.dart
+++ lib/controller/home_screen_controler.dart
@@ -12,7 +12,7 @@ class HomeScreenController with ChangeNotifier {
     isloading = true;
     notifyListeners();
     final url = Uri.parse(
-        "https://newsapi.org/v2/everything?q=tesla&from=2024-05-09&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770");
+        "https://newsapi.org/v2/everything?q=tesla&from=2024-05-10&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770");
 
     final response = await http.get(url);
 
