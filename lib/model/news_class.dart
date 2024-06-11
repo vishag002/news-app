ecfbab41f2dd49519e22ea9d402a83e8254a323a
vishag002 <vvishag477@gmail.com>
2024-06-11 17:22:43 +0530

    ontap news view

---
+++ lib/model/news_class.dart
@@ -0,0 +1,17 @@
+class News {
+  final String title;
+  final String content;
+  final String imageUrl;
+  final String author;
+  final String url;
+  final String descrition;
+
+  News({
+    required this.title,
+    required this.content,
+    required this.imageUrl,
+    required this.author,
+    required this.url,
+    required this.descrition,
+  });
+}
