0916247cd7a5101b97a2a9f890bafc412ff17f32
vishag002 <vvishag477@gmail.com>
2024-06-12 20:42:06 +0530

    api updated

--- lib/controller/search_controller.dart
+++ lib/controller/search_controller.dart
@@ -12,7 +12,7 @@ class SearchPageController with ChangeNotifier {
     isloading = true;
     notifyListeners();
     final url = Uri.parse(
-        "https://newsapi.org/v2/everything?q=$searchingData&from=2024-05-11&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770");
+        "https://newsapi.org/v2/everything?q=$searchingData&from=2024-05-12&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770");
 
     final response = await http.get(url);
     if (response.statusCode == 200) {
