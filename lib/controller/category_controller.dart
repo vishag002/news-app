4585a75203607a243113a59bee66a18fcb4ce680
vishag002 <vvishag477@gmail.com>
2024-06-22 12:37:14 +0530

    api update

--- lib/controller/category_controller.dart
+++ lib/controller/category_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:news_app/model/my_model.dart';
+import 'package:news_app/view/invalidscreen.dart';
 
 class CategryPageController with ChangeNotifier {
   Map<String, dynamic> decodeData = {};
@@ -25,7 +26,7 @@ class CategryPageController with ChangeNotifier {
     String myNews = categoryList[index].toString();
     notifyListeners();
     final url = Uri.parse(
-      "https://newsapi.org/v2/everything?q=$myNews&from=2024-05-19&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770",
+      "https://newsapi.org/v2/everything?q=$myNews&from=2024-05-22&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770",
     );
 
     final response = await http.get(url);
@@ -33,6 +34,7 @@ class CategryPageController with ChangeNotifier {
       decodeData = jsonDecode(response.body);
       objCategory = MyClass.fromJson(decodeData);
     } else {
+      InvalidErrorScreen();
       print(response.statusCode.toString());
       print("invalid API");
     }
