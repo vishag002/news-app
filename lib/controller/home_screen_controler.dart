c5aaf97db56202e7e2189866e3f6e9a5dd82ef20
vishag002 <vvishag477@gmail.com>
2024-06-22 12:37:44 +0530

    added is loading function and api error handling

--- lib/controller/home_screen_controler.dart
+++ lib/controller/home_screen_controler.dart
@@ -7,12 +7,13 @@ class HomeScreenController with ChangeNotifier {
   Map<String, dynamic> decodeData = {};
   MyClass objClass = MyClass();
   bool isloading = false;
+  bool iserror = false;
   //
   Future fetchdata() async {
     isloading = true;
     notifyListeners();
     final url = Uri.parse(
-        "https://newsapi.org/v2/everything?q=featured&from=2024-05-19&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770");
+        "https://newsapi.org/v2/everything?q=featured&from=2024-05-22&sortBy=publishedAt&apiKey=b92820ab6aea47f694544921e3535770");
 
     final response = await http.get(url);
 
@@ -26,10 +27,24 @@ class HomeScreenController with ChangeNotifier {
       InvalidErrorScreen();
     }  */
     else {
+      iserror = true;
       print(response.statusCode.toString());
       print("invalid API");
     }
     isloading = false;
     notifyListeners();
+  }
+}
+
+class ErrorScreen extends StatelessWidget {
+  const ErrorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("error screen"),
+      ),
+    );
   }
 }
